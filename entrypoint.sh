fa211ea223051100f8a63e9b4816d22a2d5066e3
Jho99999 <jhomada989@gmail.com>
2026-07-16 16:11:46 +0700

    Use Docker deployment for Railwa

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,4 +24,15 @@ a2dismod mpm_event mpm_worker >/dev/null 2>&1 || true
 rm -f /etc/apache2/mods-enabled/mpm_event.* /etc/apache2/mods-enabled/mpm_worker.* 2>/dev/null || true
 a2enmod mpm_prefork >/dev/null 2>&1 || true
 
+echo "PORT=$PORT"
+
+if [ -n "$PORT" ]; then
+    sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+
+    sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" \
+        /etc/apache2/sites-available/000-default.conf
+fi
+
+apache2ctl -S
+
 exec apache2-foreground
